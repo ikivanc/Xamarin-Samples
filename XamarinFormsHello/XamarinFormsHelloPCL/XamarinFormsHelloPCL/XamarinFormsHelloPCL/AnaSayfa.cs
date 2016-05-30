010af4168c8d1ca6953408df97f33a56e0aae322
ikivanc <ikivanc@IKIVANCL>
2016-05-30 17:33:44 +0300

    Xamarin.Forms için anasayfa eklendi

---
+++ XamarinFormsHello/XamarinFormsHelloPCL/XamarinFormsHelloPCL/XamarinFormsHelloPCL/AnaSayfa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsHelloPCL
+{
+    class AnaSayfa : ContentPage
+    {
+        Button button = new Button();
+
+        public AnaSayfa()
+        {
+            var listView = new ListView();
+            var label = new Label();
+
+            button.Text = "Tikla";
+
+            listView.ItemsSource = new string[] { "iOS 'ta çalışabiliyor", "Android'de çalışabiliyor", "Universal Windows'da çalışabiliyor", "AppleWatch'ta çalışabiliyor", "Android Wear'da çalışıyor", "Mac Masaüstünde de çalışıyor" };
+
+            listView.ItemSelected += (sender, e) =>
+            {
+                DisplayAlert(DateTime.Now.ToString(), e.SelectedItem.ToString(), "Tamam");
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Children = { label, listView, button }
+            };
+
+            button.Clicked += Button_Clicked;
+        }
+        int a = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            a++;
+            button.Text = a.ToString() + " kez tıklandı!";
+        }
+    }
+}
--- XamarinFormsHello/XamarinFormsHelloPCL/XamarinFormsHelloPCL/XamarinFormsHelloPCL/App.cs
+++ XamarinFormsHello/XamarinFormsHelloPCL/XamarinFormsHelloPCL/XamarinFormsHelloPCL/App.cs
@@ -11,20 +11,23 @@ namespace XamarinFormsHelloPCL
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+
+            MainPage = new AnaSayfa();
+            
+            //Ana Sayfayı kod ile direkt App.cs ile oluşturma
+            //MainPage = new ContentPage
+            //{
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                XAlign = TextAlignment.Center,
+            //                Text = "Welcome to Xamarin Forms!"
+            //            }
+            //        }
+            //    }
+            //};
         }
 
         protected override void OnStart()
